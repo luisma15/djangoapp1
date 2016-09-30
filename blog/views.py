ca5a7d3266b7d5ca40832d25e043b37182282c26
luisma15 <luisma15_banda@hotmail.com>
2016-09-30 10:06:33 -0600

    se dividio la pagina y se creo vinculo directo a uno

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def listar_articulos(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/listar_articulos.html', {'posts': posts})
+
+def detalle_articulo(request, pk):
+    posts = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detalle_articulo.html', {'post': posts})
