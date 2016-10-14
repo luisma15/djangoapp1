1582c60c25aa687ad52d3ccc0a27378cccc4b087
luisma15 <luisma15_banda@hotmail.com>
2016-10-14 09:50:21 -0600

    Formulario creado con insercion y edicion

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,41 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Post
+from blog.models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def listar_articulos(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #posts = Post.objects.all()
     return render(request, 'blog/listar_articulos.html', {'posts': posts})
 
 def detalle_articulo(request, pk):
     posts = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/detalle_articulo.html', {'post': posts})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.detalle_articulo', pk=post.pk)
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.detalle_articulo', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
